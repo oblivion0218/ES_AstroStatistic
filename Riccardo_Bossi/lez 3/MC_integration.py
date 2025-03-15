f49a8c0b701ef7ee82110f94206d37e1e5c2bea9
Riccardo Bossi <bossi.ricky@gmail.com>
2025-03-15 14:16:18 +0100

    .

--- Riccardo_Bossi/lez 3/MC_integration.py
+++ Riccardo_Bossi/lez 3/MC_integration.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
 from scipy.stats import norm
 
 mu = 3.4  #centroide gaussiana, ricavata dal grafico della funzione
@@ -9,47 +10,65 @@ sigma = 2 # sigma funzione
 def function(x, sigma):  # Funzione da integrare
     return x**3 * np.exp(-(x**2) / (2 * sigma**2))
 
-N = 1000000 #numero di punti per l'integrazione
-X = [] #vettore delle x generate
+N = 100000 #numero di punti per l'integrazione
 
-x_max = 20 # max integrazione
+x_max = 5*sigma # max integrazione
 x_min = 0 # min integrazione
 
+ripetizioni = 100
 
-# --------------------------------- gen gaussiana --------------------
+area_uni = []
+area_norm = []
+pos = []
+errore_rel_norm=[]
+errore_rel_uni=[]
 
-while len(X) < N : #array di punti distribuiti gaussianamente per una maggior efficienza
-    X_i = np.random.normal(mu, sigma_norm)
-    if (X_i >= x_min and X_i <= x_max):
-        X.append(X_i)
+for i in range(ripetizioni):
+    # --------------------------------- gen gaussiana --------------------
+    X = [] #vettore delle x generate
 
-X = np.array(X) # boh, va convertito in array perchè python non lo legge sennò
+    while len(X) < N : #array di punti distribuiti gaussianamente per una maggior efficienza
+        X_i = np.random.normal(mu, sigma_norm)
+        if (X_i >= x_min and X_i <= x_max):
+            X.append(X_i)
 
-peso = norm.pdf(X, mu, sigma_norm)
+    X = np.array(X) # boh, va convertito in array perchè python non lo legge sennò
 
-weighted_values = function(X, sigma) / peso # Moltiplichiamo la funzione per il peso, cioè per il reciproco della PDF
+    peso = norm.pdf(X, mu, sigma_norm)
 
-integral = np.mean(weighted_values) # Calcoliamo l'integrale
+    weighted_values = function(X, sigma) / peso # Moltiplichiamo la funzione per il peso, cioè per il reciproco della PDF
 
-# ------------------------ gen uniforme --------------------
+    integral = np.mean(weighted_values) # Calcoliamo l'integrale
 
-Y = np.random.uniform(x_min, x_max, N)
+    # ------------------------ gen uniforme --------------------
 
-integral_uni = (x_max - x_min) * np.mean(function(Y, sigma))
+    Y = np.random.uniform(x_min, x_max, N)
 
-#----------------------------------------------------------------
+    integral_uni = (x_max - x_min) * np.mean(function(Y, sigma))
 
-print ("Il risultato dell'integrale con gen normale è:" , integral )
-print ("Il risultato dell'integrale con gen uniforme è:" , integral_uni )
-print ("Il valore corretto è:" , 2*sigma**4)
+    #----------------------------------------------------------------
 
-punti = np.linspace(0,20,1000)
+    area_uni.append(integral_uni)
+    area_norm.append(integral)
+    pos.append(i+1)
 
-plt.plot(punti,20*norm(mu,sigma_norm).pdf(punti), label = "PDF gaussiana NON norm" )
-plt.plot(punti, function(punti,sigma), label= "function" , color ="Black")
+    errore_rel_norm.append( np.abs(integral - 2 * sigma**4) / (2 * sigma**4))
+    errore_rel_uni.append ( np.abs(integral_uni - 2 * sigma**4) / (2 * sigma**4))
 
-plt.xlabel("x")
-plt.ylabel("y")
+    print ("Il risultato dell'integrale con gen normale è:" , integral )
+    print ("Il risultato dell'integrale con gen uniforme è:" , integral_uni )
+    print ("Il valore corretto è:" , 2*sigma**4)
+
+plt.errorbar(pos, area_norm, yerr=errore_rel_norm, fmt='o', color='red', label='Normale', capsize=5)
+plt.errorbar(pos, area_uni, yerr=errore_rel_uni, fmt='x', color='blue', label='Uniforme', capsize=5)
+
+plt.plot(pos, area_norm, color='red', linewidth=0.5, label = "aree da dist normale")  # Linea sottile rossa
+plt.plot(pos, area_uni, color='blue', linewidth=0.5, label = "aree da dist uniform")  # Linea sottile blu
+ 
+plt.axhline(y=2 * sigma**4, color='black', linestyle='-', linewidth=2)  
+
+plt.xlabel("ripetizioni")
+plt.ylabel("valore")
 
 plt.legend()
 plt.show()
