95db731a7d885e47c2b401401b37ea07ed501c41
Riccardo Bossi <bossi.ricky@gmail.com>
2025-03-26 11:11:30 +0100

    .

--- Riccardo_Bossi/lez 3/cavallo.py
+++ Riccardo_Bossi/lez 3/cavallo.py
@@ -3,23 +3,20 @@ from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 from scipy.stats import poisson
 
-
 # Dati
 morti = np.array([0, 1, 2, 3, 4])
 N = np.array([109, 65, 22, 3, 1]) / 200  # Normalizziamo le frequenze
 
-def poisson_model(k, mu):
+def poisson(k, mu):
     return poisson.pmf(k, mu) 
 
-# Stima iniziale del parametro
-p0 = [0]
+p0 = [0] # Stima iniziale del parametro
 
-# Fit dei dati
-param, cov = curve_fit(poisson_model, morti, N, p0=p0) #curva fit restituisce il parametro ottimale del fit e la matrice di covarianza
+param, cov = curve_fit(poisson, morti, N, p0=p0) #curva fit restituisce il parametro ottimale del fit e la matrice di covarianza
 mu_opt = param
 
 # Creiamo i punti per plottare la spezzata
-y_fit = poisson_model(morti, mu_opt)
+y_fit = poisson(morti, mu_opt)
 
 # Grafico
 plt.scatter(morti, N, color='red', marker='o', label="Dati")  
