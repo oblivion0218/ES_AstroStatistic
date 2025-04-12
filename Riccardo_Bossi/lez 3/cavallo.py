42205233aba96c21a1ae843a661f1c779f89e33f
Riccardo Bossi <bossi.ricky@gmail.com>
2025-04-12 11:43:46 +0200

    english translation

--- Riccardo_Bossi/lez 3/cavallo.py
+++ Riccardo_Bossi/lez 3/cavallo.py
@@ -3,26 +3,26 @@ from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 from scipy.stats import poisson
 
-# Dati
+# Data
 morti = np.array([0, 1, 2, 3, 4])
-N = np.array([109, 65, 22, 3, 1]) / 200  # Normalizziamo le frequenze
+N = np.array([109, 65, 22, 3, 1]) / 200  # Normalize the frequencies
 
 def poisson(k, mu):
     return poisson.pmf(k, mu) 
 
-p0 = [0] # Stima iniziale del parametro
+p0 = [0] # Initial estimate of the parameter
 
-param, cov = curve_fit(poisson, morti, N, p0=p0) #curva fit restituisce il parametro ottimale del fit e la matrice di covarianza
+param, cov = curve_fit(poisson, morti, N, p0=p0) # curve fitting returns the optimal fit parameter and the covariance matrix
 mu_opt = param
 
-# Creiamo i punti per plottare la spezzata
+# Create points to plot the curve
 y_fit = poisson(morti, mu_opt)
 
-# Grafico
-plt.scatter(morti, N, color='red', marker='o', label="Dati")  
-plt.plot(morti, y_fit, color='blue', label="Fit Poissoniano")
-plt.xlabel("N° morti")
-plt.ylabel("Frequenza")
+# Plot
+plt.scatter(morti, N, color='red', marker='o', label="Data")  
+plt.plot(morti, y_fit, color='blue', label="Poisson Fit")
+plt.xlabel("Number of deaths")
+plt.ylabel("Frequency")
 plt.legend()
 plt.show()
 
